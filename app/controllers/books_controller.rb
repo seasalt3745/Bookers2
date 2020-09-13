5a9e4f37b451536458c56a5ae84203980251d7e4
vagrant <vagrant@localhost.localdomain>
2020-09-13 12:00:10 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -58,7 +58,7 @@ class BooksController < ApplicationController
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
-  	redirect_to books_path
+  	redirect_to user_path(book.user.id)
   end
 
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,4 @@ class Book < ApplicationRecord
 	belongs_to :user
 	validates :title,presence: true
 	validates :body,presence: true, length: { maximum: 200 }
-	attachment :profile_image
 end
